bd2b51e83cb5b9b9dfaeab78e9b6fe8031a5d3c7
Rahul R Kaladgi <127622361+RAHULKALADGI@users.noreply.github.com>
2023-08-13 21:51:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 // write js code here if required
+let p = document.getElementById("timer");
+let date = new Date();
+let s = `${date.getDay()}/${date.getMonth()}/${date.getFullYear()}, ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+p.innerText = s;
